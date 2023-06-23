71b7cc3194a474221bc1450348b0b6a2b8adbcfc
powder-rose <glittervodka@yandex.ru>
2023-06-23 12:29:25 +0300

    scripts added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,13 @@
-const btn = document.querySelector('.article__btn')
+const addToBagBtn = document.querySelector('.shop-btn')
+const itemImage = document.querySelector('.shop-image')
+
+const addToBag = (a) => {
+   addToBagBtn.style.backgroundColor = '#FFBDD5'
+   addToBagBtn.style.color = '#fffcfd'
+   addToBagBtn.innerHTML = 'Item added to bag'
+   itemImage.style.opacity = 0.6
+   addToBagBtn.removeEventListener("click", addToBag)
+ }
+
+addToBagBtn.addEventListener("click", addToBag)
 
